649717ddb40298b74f8a56505d3efccf95f5f086
Ahmed <ahmed.aisar@gmail.com>
2022-09-08 00:20:19 +0500

    init4

---
+++ api/index.js
@@ -0,0 +1,10 @@
+const app = require("express")();
+const bot = require("./bot");
+
+app.get("/", async (req, res) => {
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  const response = await bot();
+  res.end(response);
+});
+
+module.exports = app;
