e040e31dfe0ace3e373fd2e9b650cc50e73d7e56
Ahmed <ahmed.aisar@gmail.com>
2022-09-08 00:30:52 +0500

    init7

--- api/index.js
+++ api/index.js
@@ -1,10 +1,6 @@
 const bot = require("./bot");
 const app = require("express")();
 
-app.get("/", async (req, res) => {
-  res.redirect("/api");
-});
-
 app.get("/api", async (req, res) => {
   const response = await bot();
   res.send(response);
