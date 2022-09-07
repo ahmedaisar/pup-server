1462496cd75d55f8ece1cb1af01d22be3aa96396
Ahmed <ahmed.aisar@gmail.com>
2022-09-08 00:51:47 +0500

    init8

--- api/index.js
+++ api/index.js
@@ -1,9 +1,16 @@
-const bot = require("./bot");
+const bot = require("../lib/bot");
 const app = require("express")();
 
 app.get("/api", async (req, res) => {
   const response = await bot();
   res.send(response);
+  console.log(response);
+});
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log("Listening on port ${PORT}..");
 });
 
 module.exports = app;
